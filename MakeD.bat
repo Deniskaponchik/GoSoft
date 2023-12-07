373a2d79dedfd40cf5b5fdd727eaa79de1b6ffcf
Deniskaponchik <deniskaponchik@gmail.com>
2023-12-07 11:08:32 +0800

    Update MakeD.bat

--- MakeD.bat
+++ MakeD.bat
@@ -1,6 +1,9 @@
 cd Unifi/cmd/unifi
 
-go build -o ..\..\bin\Unifi_v3.6-TEST -ldflags="-s -w"
+rem go build -o ..\..\bin\Unifi_v3.6-TEST -ldflags="-s -w"
+rem go build -o ..\..\bin\Unifi_v3.7-TEST -ldflags="-s -w"
+go build -o ..\..\bin\Unifi_v3.8-TEST -ldflags="-s -w"
 
 rem start /B ../../bin/Unifi_v3.6-TEST -mode PROD -time +5 -httpUrl 10.57.179.121:8081 -db it_support_db_3
-start /B ../../bin/Unifi_v3.6-TEST -mode PROD -time +5 -httpUrl 10.57.179.121:8081 -db it_support_db_3
+rem start /B ../../bin/Unifi_v3.7-TEST -mode PROD -time +5 -httpUrl 10.57.179.121:8081 -db it_support_db_3
+start /B ../../bin/Unifi_v3.8-TEST -mode PROD -time +5 -httpUrl 10.57.179.121:8081 -db it_support_db_3
